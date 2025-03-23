4ab2be192b963d01a9715d43533c0a1e4e5cf652
Shadrack mballah <shadrackmballah80@gmail.com>
2025-03-23 18:25:40 +0300

    Add dashboard overview component with attendance statistics and trends

--- resources/views/livewire/admin/admin-dashboard.blade.php
+++ resources/views/livewire/admin/admin-dashboard.blade.php
@@ -1,3 +1,5 @@
 <div>
-    {{-- Stop trying to control. --}}
+    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+        <livewire:dashborad-widget-overview />
+    </div>
 </div>
--- resources/views/livewire/teacher/students/add-student.blade.php
+++ resources/views/livewire/teacher/students/add-student.blade.php
@@ -52,7 +52,7 @@
 
                     <div class="mb-4 sm:mb-8">
                         <label for="hs-feedback-post-comment-email-1"
-                            class="block mb-2 text-sm font-medium dark:text-white">Email address</label>
+                            class="block mb-2 text-sm font-medium dark:text-white">Age</label>
                         <input type="number" wire:model="age" id="hs-feedback-post-comment-email-1"
                             class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 bg-gray-50 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
                             placeholder="Enter Age">
