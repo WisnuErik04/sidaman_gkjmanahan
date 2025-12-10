1ebad295dca33d935a10182d333c0cc237e54808
Erik Wisnu <erikwnugroho@gmail.com>
2025-12-10 10:03:13 +0700

    101225 V3

--- resources/views/livewire/admin/export-anggota/list-anggota.blade.php
+++ resources/views/livewire/admin/export-anggota/list-anggota.blade.php
@@ -198,9 +198,13 @@
                                             <td class="h-px w-auto whitespace-nowrap">
                                                 <div class="px-6 py-2">
                                                     <span class="text-sm text-gray-800 dark:text-neutral-200">
-                                                        {{ $anggota->status->name ?? '-' }} 
-                                                        {{ $tanggalLahir = Carbon::parse($anggota->tgl_lahir);
-                $usia = $tanggalLahir->diffInYears(Carbon::now()); }}
+                                                        {{ $anggota->status->name ?? '-' }}
+                                                        @php
+                                                            $tanggalLahir = \Carbon\Carbon::parse($anggota->tgl_lahir);
+                $usia = $tanggalLahir->diffInYears(\Carbon\Carbon::now()); 
+                                                            
+                                                        @endphp
+                                                        {{ $usia }}
                                                         </span>
                                                 </div>
                                             </td>
